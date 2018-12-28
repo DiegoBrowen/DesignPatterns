8aa979a7edb9df164a6aff579a20f4319c2787a0
Diego Browen Proença de Oliveira <diegop.o@hotmail.com>
2018-12-28 20:01:27 -0200

    Adicionado command de ligar stereo e colcoar um cd

---
+++ src/DesignPatterns.Command.RemoteControl/Stereo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Command.RemoteControl
+{
+    public class Stereo
+    {
+        public int Volume { get; private set; }
+
+        public string On()
+        {
+            return "Stero on";
+        }
+
+
+        public string SetCd()
+        {
+            return "Set cd";
+        }
+
+        public string SetVolume(int volume)
+        {
+            Volume = volume;
+            return $"Set volume {volume}";
+        }
+    }
+}
---
+++ src/DesignPatterns.Command.RemoteControl/StereoOnWithCdCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Command.RemoteControl
+{
+    public class StereoOnWithCdCommand : ICommand
+    {
+        private readonly Stereo _stereo;
+        public string Result { get; private set; }
+
+        public StereoOnWithCdCommand(Stereo stereo)
+        {
+            _stereo = stereo;
+        }
+        public void Execute()
+        {
+            var result = new StringBuilder();
+            result.AppendLine(_stereo.On());
+            result.AppendLine(_stereo.SetCd());
+            result.AppendLine(_stereo.SetVolume(11));
+            Result = result.ToString();
+        }
+    }
+}
